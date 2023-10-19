2e1de3a8d2944fe84489b977f234c7c53fb46310
drophang <victor1998kombad@gmail.com>
2023-10-19 11:36:34 -0400

    Inicio

--- src/ManejodeString.java
+++ src/ManejodeString.java
@@ -2,10 +2,6 @@ public class ManejodeString {
     public static void main(String[] args) {
         String S = "Programacion orientada a obejetos";
         System.out.println("Tamaño " + S.length());
-        for (int i = 0; i < S.length();i++){
-            if (S.charAt(i) == ' '){
-                ;
-            }
-        }
+        System.out.println(S.trim());
     }
 }

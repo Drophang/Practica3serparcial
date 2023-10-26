56b76d09f33fa4806fe34ce262b229aa341b8c93
drophang <victor1998kombad@gmai.com>
2023-10-26 08:49:01 -0400

    Arreglo_Espacios

--- src/ManejodeString.java
+++ src/ManejodeString.java
@@ -2,6 +2,8 @@ public class ManejodeString {
     public static void main(String[] args) {
         String S = "Programacion orientada a obejetos";
         System.out.println("Tamaño " + S.length());
-        System.out.println(S.trim());
+        S = S.replaceAll(" ","");
+        System.out.println(S);
+        
     }
 }

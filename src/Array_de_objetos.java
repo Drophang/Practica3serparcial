f1b21a3157dd40e3cdb89906b766631a55b8ffa8
drophang <victor1998kombad@gmai.com>
2023-10-26 10:46:35 -0400

    ArregloArraycompleto

--- src/Array_de_objetos.java
+++ src/Array_de_objetos.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 public class Array_de_objetos{
    public static void main(String[] args) {
-    Tienda T[];
-    
+    ArrayList<Tienda> Ti = new ArrayList<>();
+    Tienda T = new Tienda("pepepc", "pepe", 3011);
+    Ti.add(T);
    }
 }
--- src/Tienda.java
+++ src/Tienda.java
@@ -1,11 +1,21 @@
+import java.util.ArrayList;
 public class Tienda{
     String Nombre;
     String Propietario;
     int identificador;
-
+    ArrayList<Computadoras> C = new ArrayList();
     public Tienda(String Nombre,String Propietario,int identificador){
         this.Nombre = Nombre;
         this.Propietario = Propietario;
         this.identificador = identificador;
+        this.C = C;
+    }
+    public void addcomputador1(){
+        Computadoras compu1 = new Computadoras ("pepe",1.000,"core","Mac",500);
+        C.add(compu);
+    }
+    public void addcomputador2(){
+        Computadoras compu2 = new Computadoras ("pro",2.000,"itm","Mac",500);
+        C.add(compu);
     }
 }

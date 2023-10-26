209f9a1bcea5b42acd0073c4be1ba04e700908f1
drophang <victor1998kombad@gmai.com>
2023-10-26 09:00:38 -0400

    Terminado_3.2

--- src/ManejodeString.java
+++ src/ManejodeString.java
@@ -4,6 +4,13 @@ public class ManejodeString {
         System.out.println("Tamaño " + S.length());
         S = S.replaceAll(" ","");
         System.out.println(S);
-        
+        S = S.toUpperCase();
+        System.out.println(S);
+        S = S + "12345";
+        System.out.println(S);
+        String Sub_S = S.substring(10,15);
+        System.out.println(Sub_S);
+        S = S.replace("o","O");
+        S.compareTo("Programacion");
     }
 }
